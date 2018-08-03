80a88ebda2fb7d3ee40d513417a04c864f7d1642
Artur Bill <artur.bill@web.de>
2018-08-03 23:21:31 +0200

    Added Person Component

--- pr2/src/App.js
+++ pr2/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hallo there, this is me, Fritz!</h1>
+        <Person />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hallo there, this is me, Hans!'));
---
+++ pr2/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Person = () => {
+    return <p>Me is a Person.</p>;
+}
+
+export default Person;
