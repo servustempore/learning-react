5529a27c257cd65e8252d340112a39ef3f146fe8
Artur Bill <artur.bill@web.de>
2018-08-04 00:14:24 +0200

    added click event to button

--- pr2/src/App.js
+++ pr2/src/App.js
@@ -4,6 +4,7 @@ import Person from './Person/Person';
 
 class App extends Component {
   state = {
+    counter: 0,
     persons: [
       {name: "Hans", age: 22},
       {name: "Peter", age: 19},
@@ -15,10 +16,15 @@ class App extends Component {
     return <Person name={person.name} age={person.age} />;
   }
 
+  clickMe = () => {
+    console.log("Clicked!");
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Hallo there, this is me, Fritz!</h1>
+        <h1>Hallo there, look at sese persons pls!</h1>
+        <button onClick={this.clickMe}>Klick mi!</button>
         {this.state.persons.map(this.personify)}
       </div>
     );
