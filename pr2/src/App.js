d72b041665bc1f0bc29025195afe6b48c6a705be
Artur Bill <artur.bill@web.de>
2018-08-04 21:48:41 +0200

    ternary conditional JSX Rendering

--- pr2/src/App.js
+++ pr2/src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
       {name: "Hans", age: 22},
       {name: "Peter", age: 19},
       {name: "Peer", age: 24}
-    ]
+    ],
+    showPerson: true
   };
 
   clickMe = () => {
@@ -44,6 +45,11 @@ class App extends Component {
     return <Person name={person.name} age={person.age} click={this.clickPerson.bind(this, person.name)} changeName={this.changeName}/>;
   }
 
+  togglePersons = () => {
+    const showPersons = this.state.showPerson;
+    this.setState({showPerson: !showPersons});
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -58,7 +64,12 @@ class App extends Component {
         <h1>Hallo there, look at sese persons pls!</h1>
         <p>Counter: {this.state.counter}</p>
         <button style={style} onClick={this.clickMe}>Klick mi!</button>
-        {this.state.persons.map(this.personify)}
+        <p>Show Persons: </p>
+        <button className="button-cstm" onClick={this.togglePersons}>Toggle Persons</button>
+        { this.state.showPerson ?
+          this.state.persons.map(this.personify) :
+          null
+        }
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hallo there, this is me, Hans!'));
