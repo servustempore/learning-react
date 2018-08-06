6bbb42e2ab29c00df7c8afd1a2c6df825019c7eb
Artur Bill <artur.bill@web.de>
2018-08-06 14:27:59 +0200

    added burger builder / fixex config

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <p>Testico</p>
         <Layout>
-          <p>Testico</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
