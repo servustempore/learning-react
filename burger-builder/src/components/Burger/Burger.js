3bf71e89390cf9d8ac279af78c6c74605205a4ef
Artur Bill <artur.bill@web.de>
2018-08-06 16:18:41 +0200

    added Ingredient-Iterator for building Ingredient Array

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,12 +3,22 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingKey => {
+            return [...Array(props.ingredients[ingKey])]
+                .map((_, i) => {
+                    return <BurgerIngredient key={ingKey + i} type={ingKey} />;
+                });
+        })
+        .reduce((allIngredients, currIngredient) => {
+            return allIngredients.concat(currIngredient);
+        }, []);
+
     return (
         <div className={classes.Burger}>
+
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients.length ? transformedIngredients: "Yo WTF put some cheese in there or smth!"}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
